5a49bbe0a555b875599aac55a00b1c47a0d04d15
Rahul Choudhary <1122rahulchoudhary@gmail.com>
2026-04-10 18:15:11 +0530

    Update database connection details

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
+  host: "mysql://root:AIsDSUJiNtCTjoumSUopryDRkOBpJlXM@mainline.proxy.rlwy.net:30103/railway",
   user: "root",
   password: "root",
   database: "school_db",
